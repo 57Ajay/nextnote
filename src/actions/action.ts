57467334a7c78203388d05251ba0140476fad089
Ajay Upadhyay <57ajay.u@gmail.com>
2024-05-26 17:06:14 +0530

    added some error handlings

--- src/actions/action.ts
+++ src/actions/action.ts
@@ -1,14 +1,25 @@
 'use server'
 
 import prisma from "@/lib/db";
+import { revalidatePath } from "next/cache";
 
-export default async function CreatePost(formData : FormData){
-    const title = formData.get("title") as string;
-    const body = formData.get("body") as string;
+
+export default async function CreatePost(formData: FormData) {
+  const title = formData.get("title") as string;
+  const body = formData.get("body") as string;
+
+  try {
     await prisma.post.create({
-        data:{
-            title,
-            body
-        }
-    })
+      data: {
+        title,
+        body,
+      },
+    });
+
+    revalidatePath("/posts");
+
+  } catch (error) {
+    console.error("Error creating post:", error);
+    return new Response("Error creating post", { status: 500 });
+  }
 }
