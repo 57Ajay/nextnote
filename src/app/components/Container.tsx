81e84b4f59cbb5342e9cf60e9b3110b964b94c7a
Ajay Upadhyay <57ajay.u@gmail.com>
2024-05-26 17:32:45 +0530

    fixing some bugs

--- src/app/components/Container.tsx
+++ src/app/components/Container.tsx
@@ -1,7 +1,7 @@
 
 export default function Container({ children } : { children: React.ReactNode }) {
   return (
-    <div className="max-w-[1080px] mx-auto px-4 min-h-screen shadow-lg bg-zinc-100">
+    <div className="max-w-[1240px] mx-auto px-4 min-h-screen shadow-lg bg-zinc-100">
         {children}
     </div>
   )
